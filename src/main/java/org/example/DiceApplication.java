1a1c9a0d5199b00060d343c7cddca41e7ee120a1
Carlos <solrak_27@hotmail.com>
2025-08-16 16:06:10 +0200

    Configurada API key de honeycomb, ejecutar comando para el java agent

---
+++ src/main/java/org/example/DiceApplication.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiceApplication {
+    public static void main(String[] args) {
+
+        SpringApplication app = new SpringApplication(DiceApplication.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
+
+    }
+}
